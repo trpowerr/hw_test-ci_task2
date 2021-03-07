739ae2a04e4a4a878b455776b8e86af3515e207a
Tryakin Mikhail <mixazbubl@yandex.ru>
2021-03-07 21:51:50 +0300

    fix

--- src/__test__/arr.test.js
+++ src/__test__/arr.test.js
@@ -9,3 +9,13 @@ test('should check the order ', () => {
   ];
   expect(received).toEqual(expected);
 });
+
+test('should check the order ', () => {
+  const received = personArr;
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(received).not.toBe(expected);
+});
